22b109fbd856846d13e2066b0c8e87a4f67870aa
ChoiDaejin <dejeee22@naver.com>
2025-02-12 12:58:19 +0900

    b

--- xr-fundamentals/part1/app/src/main/java/com/example/android/xrfundamentals/MainActivity.kt
+++ xr-fundamentals/part1/app/src/main/java/com/example/android/xrfundamentals/MainActivity.kt
@@ -25,6 +25,7 @@ import com.example.android.xrfundamentals.ui.theme.XRFundamentalsTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         enableEdgeToEdge()
         setContent {
             XRFundamentalsTheme {
